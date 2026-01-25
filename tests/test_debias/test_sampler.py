2c5145f60fa55680e7d13a69fd0f1ff7ed98accb
hamletkhachatryan <hamletkhachatryann@gmail.com>
2026-01-25 20:17:44 +0000

    Fixing testing

--- tests/test_debias/test_sampler.py
+++ tests/test_debias/test_sampler.py
@@ -12,12 +12,15 @@ def test_stochastic_sampler(pdb_file_param: str):
     assert isinstance(ids, list)
     assert len(ids) > 0
     selectors = ids[:2]
+    selection_string = ",".join(
+        ["chain {} and resid {}".format(i[0], i[1]) for i in selectors]
+    )
     selections = sample.stochastic_omission_sampler(
         pdb_file_param,
         omit_type="amino_acids",
         omit_fraction=0.3,
         n_iterations=2,
-        always_omit=selectors,
+        always_omit=selection_string,
         seed=42,
     )
 
@@ -68,12 +71,16 @@ def test_always_omit_with_missing_selectors(pdb_file_param: str):
     fake_selector_1 = ("Z", 99999, "FOO")
     fake_selector_2 = ("Z", 99998, "BAR")
     selectors = [real_selector, fake_selector_1, fake_selector_2]
+    selection_string = ",".join(
+        ["chain {} and resid {}".format(i[0], i[1]) for i in selectors]
+    )
+
     selections = sample.stochastic_omission_sampler(
         pdb_file_param,
         omit_type="amino_acids",
         omit_fraction=0.3,
         n_iterations=1,
-        always_omit=selectors,
+        always_omit=selection_string,
         seed=7,
     )
     assert isinstance(selections, list)
